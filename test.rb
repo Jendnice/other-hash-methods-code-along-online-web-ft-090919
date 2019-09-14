29ebb14fdfe33bd64dbcf323fd198de762ba8b31
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 17:44:36 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_min_2(groceries)
+def get_the_min_2
   groceries = {
 dairy: ["milk", "yogurt", "cheese"],
 vegetable: ["carrots", "broccoli", "cucumbers"],
